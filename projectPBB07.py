fee5223939b59cead7c991e1b992fd93912cb73c
LAPTOP-1N0NQPCN\Kummarapurugu Ramesh <rameshkumma99@gmail.com>
2023-12-09 16:56:24 +0530

    first commit

---
+++ projectPBB07.py
@@ -0,0 +1,82 @@
+ #Gender detection and Age prediction Main Project 
+#by PBB07 batch members 
+#SWARNANDHRA COLLEGE OF ENGINEERING AND TECHNOLOGY
+
+import cv2
+import math
+import argparse
+
+def highlightFace(net, frame, conf_threshold=0.7):
+    frameOpencvDnn=frame.copy()
+    frameHeight=frameOpencvDnn.shape[0]
+    frameWidth=frameOpencvDnn.shape[1]
+    blob=cv2.dnn.blobFromImage(frameOpencvDnn, 1.0, (300, 300), [104, 117, 123], True, False)
+    #image,scalefactor,size224x224,227x227,299x299,subtractmeanvalues(r=103.93,g=116.77,b=123.68),swapRB(openCV BGR to mean RGB )
+    net.setInput(blob)
+    detections=net.forward()
+    faceBoxes=[]
+    for i in range(detections.shape[2]):
+        confidence=detections[0,0,i,2]
+        if confidence>conf_threshold:
+            x1=int(detections[0,0,i,3]*frameWidth)
+            y1=int(detections[0,0,i,4]*frameHeight)
+            x2=int(detections[0,0,i,5]*frameWidth)
+            y2=int(detections[0,0,i,6]*frameHeight)
+            faceBoxes.append([x1,y1,x2,y2])
+            cv2.rectangle(frameOpencvDnn, (x1,y1), (x2,y2), (0,255,0), int(round(frameHeight/150)), 8)
+            #image,startpoint,endpoint,color(BGR),thickness,return image value
+    return frameOpencvDnn,faceBoxes
+parser=argparse.ArgumentParser()
+parser.add_argument('--image')
+
+args=parser.parse_args()
+
+faceProto="pbtxt_facedetector.pbtxt"
+faceModel="pb_facedetector.pb"
+ageProto="agedesign.prototxt"
+ageModel="agemodel.caffemodel"
+genderProto="genderdesign.prototxt"
+genderModel="gendermodel.caffemodel"
+
+MODEL_MEAN_VALUES=(78.4263377603, 87.7689143744, 114.895847746)
+ageList=['(0-2)', '(4-6)', '(8-12)', '(15-20)', '(25-32)', '(38-43)', '(48-53)', '(60-100)']
+genderList=['Male','Female']
+
+faceNet=cv2.dnn.readNet(faceModel,faceProto)
+# to load network into memory
+ageNet=cv2.dnn.readNet(ageModel,ageProto)
+genderNet=cv2.dnn.readNet(genderModel,genderProto)
+
+video=cv2.VideoCapture(args.image if args.image else 0)
+padding=20
+#wait for 1ms or key pressed here wait 1ms if 0 wait till key pressed to display image
+while cv2.waitKey(1)<0 :
+    hasFrame,frame=video.read()
+    if not hasFrame:
+        cv2.waitKey()
+        break
+    
+    resultImg,faceBoxes=highlightFace(faceNet,frame)
+    if not faceBoxes:
+        print("No face detected")
+
+    for faceBox in faceBoxes:
+        face=frame[max(0,faceBox[1]-padding):
+                   min(faceBox[3]+padding,frame.shape[0]-1),max(0,faceBox[0]-padding)
+                   :min(faceBox[2]+padding, frame.shape[1]-1)]
+#img.shape(0=height,1=width,2=channels(alpha,r,g,b))
+        blob=cv2.dnn.blobFromImage(face, 1.0, (227,227), MODEL_MEAN_VALUES, swapRB=False)
+        genderNet.setInput(blob)
+        genderPreds=genderNet.forward()
+        gender=genderList[genderPreds[0].argmax()]
+        print(f'Gender: {gender}')
+
+        ageNet.setInput(blob)
+        agePreds=ageNet.forward()
+        age=ageList[agePreds[0].argmax()]
+        print(f'Age: {age[1:-1]} years')
+        #slicing index 1 to last but one
+
+        cv2.putText(resultImg, f'{gender}, {age}', (faceBox[0], faceBox[1]-10), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,255,255), 2, cv2.LINE_AA)
+        #image,text,org,font,fontScale,color in rgb(255,255,0)=yellow,thickness.linetype,bottomLeftOrigin
+        cv2.imshow("Detecting age and gender", resultImg)
